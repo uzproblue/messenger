c5beaa798c8eea23122346d8f84f2c56580880ac
Firdavs Rakhmonov <48709598+uzproblue@users.noreply.github.com>
2023-07-04 01:06:43 +0200

    Header component

---
+++ app/actions/getMessages.ts
@@ -0,0 +1,22 @@
+const getMessages = async (conversationId: string) => {
+  try {
+    const messages = await prisma?.message.findMany({
+      where: {
+        conversationId: conversationId,
+      },
+      include: {
+        sender: true,
+        seen: true,
+      },
+      orderBy: {
+        createdAt: "asc",
+      },
+    });
+
+    return messages;
+  } catch (error: any) {
+    return [];
+  }
+};
+
+export default getMessages;
